693d7904b024fdf5a3202585b4d19879417bf0bb
GDami <41579255+GDami@users.noreply.github.com>
2025-05-07 12:37:52 +0200

    added a list of current needed callback, added a state field for applications

--- suivi_candidatures/findjob/models.py
+++ suivi_candidatures/findjob/models.py
@@ -11,11 +11,17 @@ class Callback(models.Model):
     message = models.fields.TextField(max_length=2000)
 
 class Application(models.Model):
+    class ApplicationState(models.TextChoices):
+        OPEN = 'OP'
+        DECLINED = 'NO'
+        ACCEPTED = 'OK'
+
     title = models.fields.CharField(max_length=100)
     link = models.fields.URLField(null=False,blank=False)
     applied = models.fields.BooleanField(default=True)
     date_applied = models.fields.DateField(null=True, blank=True)
     cover_letter = models.fields.TextField(max_length=2000, default="", null=True)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
+    state = models.fields.CharField(choices=ApplicationState.choices, max_length=2, default=ApplicationState.OPEN)
     called_back = models.fields.BooleanField(default=False)
     callback = models.OneToOneField(Callback, null=True, blank=True, on_delete=models.SET_NULL)
--- suivi_candidatures/findjob/views.py
+++ suivi_candidatures/findjob/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime, timedelta
 
 from findjob.models import Application, Company
 from findjob.forms import AddApplicationForm, CompanyForm, AddCallbackForm
 
 def application_list(request):
     applications = Application.objects.all().order_by('date_applied')
+    need_callback = []
+    now = datetime.now().date()
+
+    for application in applications:
+        should_callback = now - application.date_applied >= timedelta(7)
+        if application.state == Application.ApplicationState.OPEN and not application.called_back and should_callback:
+            need_callback.append(application)
 
     return render(request,
                   'findjob/application_list.html',
-                  {'applications':applications}
+                  {'applications':applications, 'need_callback':need_callback}
                   )
 
 def application_detail(request, id):
@@ -34,6 +42,28 @@ def application_add(request):
                   'findjob/application_add.html',
                   {'form':form}
                   )
+
+def application_delete(request, id):
+    application = get_object_or_404(Application, id=id)
+
+    return render(request,
+                  'findjob/application_delete',
+                  {'application':application}
+                  )
+
+def application_accepted(request, id):
+    application = get_object_or_404(Application, id=id)
+    application.state = Application.ApplicationState.ACCEPTED
+    application.save()
+
+    return redirect('application-detail', application.id)
+
+def application_declined(request, id):
+    application = get_object_or_404(Application, id=id)
+    application.state = Application.ApplicationState.DECLINED
+    application.save()
+
+    return redirect('application-detail', application.id)
 
 def company_list(request):
     companies = Company.objects.all().order_by('name')
